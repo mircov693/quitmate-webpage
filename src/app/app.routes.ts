21fe916d11f5821fc861cc7fced86cd7f0f224d5
Vladimir Cabacov <nfalter1993@gmail.com>
2024-11-16 12:38:02 +0100

    Use / as a home route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,11 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 import {HomeComponent} from './home/home.component';
 import {PrivacyComponent} from './privacy/privacy.component';
 import {SupportComponent} from './support/support.component';
 
 export const routes: Routes = [
-  { path: 'privacy', title: 'Privacy', component: PrivacyComponent },
-  { path: 'support', title: 'Support', component: SupportComponent },
-  { path: 'home', title: 'Home', component: HomeComponent },
-  { path: '**', title: 'Home', component: HomeComponent }
+  {path: '', title: 'Home', component: HomeComponent},
+  {path: 'privacy', title: 'Privacy', component: PrivacyComponent},
+  {path: 'support', title: 'Support', component: SupportComponent},
+  {path: '**', title: 'Home', component: HomeComponent}
 ];
