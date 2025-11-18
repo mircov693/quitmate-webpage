58644ff3a7f34edea78e9f8f7b48743624ad582b
Vladimir Cabacov <nfalter1993@gmail.com>
2025-11-18 01:46:51 +0100

    Fix error

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-document.querySelector('.burger-menu').addEventListener('click', showHideMenu);
-
 function showHideMenu() {
     const menu = document.getElementById("menu");
     const isOpen = menu.classList.toggle("open");
     document.querySelector('.burger-menu').setAttribute('aria-expanded', isOpen.toString());
 }
+
+document.querySelector('.burger-menu')?.addEventListener('click', showHideMenu);
