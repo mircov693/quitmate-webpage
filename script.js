330b3f541ce8877c8ba9658d5abd9572f648dcdd
Vladimir Cabacov <nfalter1993@gmail.com>
2025-11-27 21:56:45 +0100

    Add multilingual support for EN, DE

--- script.js
+++ script.js
@@ -8,3 +8,33 @@ function showHideMenu() {
 
 document.querySelector('.burger-menu').addEventListener('click', showHideMenu);
 document.getElementById("year").textContent = new Date().getFullYear().toString()
+
+function updateContent(langData) {
+    document.querySelectorAll('[data-i18n]').forEach(element => {
+        const key = element.getAttribute('data-i18n');
+        element.innerHTML = langData[key];
+    });
+}
+
+function setLanguagePreference(lang) {
+    localStorage.setItem('language', lang);
+    location.reload();
+}
+
+async function fetchLanguageData(lang) {
+    const response = await fetch(`languages/${lang}.json`);
+    return response.json();
+}
+
+async function changeLanguage(lang) {
+    await setLanguagePreference(lang);
+
+    const langData = await fetchLanguageData(lang);
+    updateContent(langData);
+}
+
+window.addEventListener('DOMContentLoaded', async () => {
+    const userPreferredLanguage = localStorage.getItem('language') || 'en';
+    const langData = await fetchLanguageData(userPreferredLanguage);
+    updateContent(langData);
+});
